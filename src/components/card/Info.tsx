019480ce2df740b3d89585be7b10d0f909770e42
giorgigongladze <giorgi.gongladze.1@btu.edu.ge>
2023-06-20 20:44:53 +0400

    CardComponent

---
+++ src/components/card/Info.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: row; 
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const InfoBox = styled.div`
+  padding: 8px 27px;
+  background-color: #f0f0f0;
+  height: 29px;
+  border-radius: 30px;
+  background-color: red;
+`;
+
+const InfoText = styled.span`
+  font-size: 11px;
+  display: flex;
+  justify-content: center;
+  font-weight: bold;
+`;
+
+function Info() {
+  return (
+    <div>
+    <InfoContainer>
+      <InfoBox>
+        <InfoText>$ 10/Hour</InfoText>
+      </InfoBox>
+      <InfoBox>
+        <InfoText>Part-Time</InfoText>
+      </InfoBox>
+      <InfoBox>
+        <InfoText>Expert</InfoText>
+      </InfoBox>
+    </InfoContainer>
+    </div>
+  );
+}
+
+export default Info;
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { styled } from 'styled-components';
+import Card from '../components/card/Card';
+
+
 
 function Home() {
   return (
+    <div>
     <div>Home</div>
+    <Card image={''} />
+
+    </div>
   )
 }
 
-export default Home
+export default Home;
 
