18419178bfa3794cbd0b29a167827ad61681634a
giorgigongladze <giorgi.gongladze.1@btu.edu.ge>
2023-06-21 20:07:01 +0400

    CardDetails

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { styled } from 'styled-components';
 
 import MainComponent from '../components/main_component/MainComponent';
-import SearchBar from '../components/SearchBar/SearchhBar';
 
 function Home() {
   return (
