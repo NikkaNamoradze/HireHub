bacc1c8df4c393717f67b00759230f512e01e3ac
giorgigongladze <giorgi.gongladze.1@btu.edu.ge>
2023-06-23 21:16:50 +0400

    Filter

---
+++ src/components/Filter/CheckBoxLabel.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+interface CheckboxLabelProps {
+  children: string;
+}
+
+const CheckboxLabel = ({ children }: CheckboxLabelProps) => {
+  return (
+    <Label>
+      <CheckboxInput type="checkbox" />
+      {children}
+    </Label>
+  );
+};
+
+const Label = styled.label`
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  margin-top: 15px;
+`;
+
+const CheckboxInput = styled.input`
+  margin-right: 8px;
+  accent-color: #222222;
+`;
+
+
+
+export default CheckboxLabel;
+
